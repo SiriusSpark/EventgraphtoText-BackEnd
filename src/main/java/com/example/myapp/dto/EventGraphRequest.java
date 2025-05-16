421dbf9f6b1341cd70308e388da9edde175db751
SiriusSpark <100188787+SiriusSpark@users.noreply.github.com>
2025-05-16 13:56:53 +0800

    Delete src directory

--- src/main/java/com/example/myapp/dto/EventGraphRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapp.dto;
-
-import lombok.Data;
-
-@Data
-public class EventGraphRequest {
-    private String title;
-    private String description;
-} 
--- src/main/java/com/example/myapp/enums/RelationStrength.java
+++
@@ -1,7 +0,0 @@
-package com.example.myapp.enums;
- 
-public enum RelationStrength {
-    低,
-    中,
-    高
-} 
